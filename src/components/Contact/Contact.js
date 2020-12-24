4c7d3dbf87d2f62db050757aabe72f5097e1fde3
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-12-24 16:51:57 -0500

    Added container to contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './Contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub,  faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 class Contact extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {};
     }
@@ -14,21 +14,21 @@ class Contact extends Component {
 
         return (
             <div className='Contact'>
-                <div className='logo'>
-                    <div className='gitubLogo'>
-                        <a className='logo' href='https://github.com/Sp198418' target='blank'>{<FontAwesomeIcon icon={faGithub} size='lg' />}</a>
-                    </div>
-                    <div className='gitubLogo'>
-                        <a className='logo' href='https://www.linkedin.com/in/samuelpl/' target='blank'>{<FontAwesomeIcon icon={faLinkedin} size= 'lg' />}</a>
-                    </div>
+                <div className="Container">
+                    <div className='logo'>
+                        <div className='gitubLogo'>
+                            <a className='logo' href='https://github.com/Sp198418' target='blank'>{<FontAwesomeIcon icon={faGithub} size='lg' />}</a>
+                        </div>
+                        <div className='gitubLogo'>
+                            <a className='logo' href='https://www.linkedin.com/in/samuelpl/' target='blank'>{<FontAwesomeIcon icon={faLinkedin} size='lg' />}</a>
+                        </div>
 
-                <a href="mailto:samuelpierrelouis10@gmail.com">
-                    <h2> Email:samuelpierrelouis10@gmail.com 
+                        <a href="mailto:samuelpierrelouis10@gmail.com">
+                            <h2> Email:samuelpierrelouis10@gmail.com
                       </h2>
-                </a>
-
-
-            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
 
         )
